cd6de9619950c115e0df25777d9a08c98bd3b7d9
mkasahar <parasolgirlcook@yahoo.co.jp>
2023-05-27 12:55:22 +0900

    Debug Controller fix

--- src/main/java/com/mkasahar/sampleteacher/SampleController.java
+++ src/main/java/com/mkasahar/sampleteacher/SampleController.java
@@ -1,21 +1,13 @@
 package com.mkasahar.sampleteacher;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-    @Value("${sample.val:ERR}")
-    String val;
 
     @RequestMapping(value = "/hello")
     public String hello() {
         return "Hello World!";
-    }
-
-    @RequestMapping(value = "/val")
-    public String val() {
-        return val;
     }
 }
